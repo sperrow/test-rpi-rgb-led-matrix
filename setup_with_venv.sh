149de739ced2acd386d4f40e2c6465c071facbda
Matthew Sperry <mattjsperry@gmail.com>
2024-07-23 23:41:27 -0700

    separate setup for venv

--- setup_with_venv.sh
+++ setup_with_venv.sh
@@ -1,3 +1,9 @@
+echo "Creating python virtual environment:"
+venv_directory=testmatrix_venv
+python3 -m venv $venv_directory --system-site-packages
+source $venv_directory/bin/activate
+echo "Virtual environment created"
+
 install_path=$(pwd)
 systemd_name=testmatrix
 
@@ -9,7 +15,7 @@ echo "...done"
 
 echo "Creating systemd service:"
 sudo cp ./config/$systemd_name.service /etc/systemd/system/
-sudo sed -i -e "/\[Service\]/a ExecStart=python3 ${install_path}/runtext.py" /etc/systemd/system/$systemd_name.service
+sudo sed -i -e "/\[Service\]/a ExecStart=${install_path}/${venv_directory}/bin/python3 ${install_path}/runtext.py" /etc/systemd/system/$systemd_name.service
 sudo systemctl daemon-reload
 sudo systemctl start $systemd_name
 sudo systemctl enable $systemd_name
