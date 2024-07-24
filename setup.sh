090eadc7e41444872ab8e78ea51024e7216cb63b
Matthew Sperry <mattjsperry@gmail.com>
2024-07-23 19:44:57 -0700

    clean up setup

--- setup.sh
+++ setup.sh
@@ -4,10 +4,17 @@ python3 -m venv $venv_directory --system-site-packages
 source $venv_directory/bin/activate
 echo "Virtual environment activated"
 
-echo "Creating systemd service:"
 install_path=$(pwd)
 systemd_name=testmatrix
-sudo cp ./config/testmatrix.service /etc/systemd/system/
+
+echo "Removing systemd service if it exists:"
+sudo systemctl stop $systemd_name
+sudo rm -rf /etc/systemd/system/$systemd_name.*
+sudo systemctl daemon-reload
+echo "...done"
+
+echo "Creating systemd service:"
+sudo cp ./config/$systemd_name.service /etc/systemd/system/
 sudo sed -i -e "/\[Service\]/a ExecStart=${install_path}/${venv_directory}/bin/python3 ${install_path}/runtext.py" /etc/systemd/system/$systemd_name.service
 sudo systemctl daemon-reload
 sudo systemctl start $systemd_name
